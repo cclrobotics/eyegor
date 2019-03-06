c5082fe2de3619f3fc463439aa426627a26917a5
tdobbs <timdobbs@gmail.com>
2019-03-05 19:51:56 -0800

    added some test photos for precision location-finding

--- testing_files/takepic.sh
+++ testing_files/takepic.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-fswebcam --flip h,v --resolution 1920x1080 --save ~/eyegor/testing_files/test.jpg
+fswebcam --flip v --resolution 1280x720 --set brightness=50% --set "Focus, auto"=False --set "Focus (absolute)"=0 --save ~/eyegor/testing_files/plate1_final_X4Y-2.jpg
